ae7cafac7c8aa11f1ba5ea8c62d8e1ca23d3d6e5
Madina89 <madinalinux89@gmail.com>
2019-08-01 14:13:00 -0500

    name

--- docker.tf
+++ docker.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "docker" {
   ami                         = "${var.ami}"
   key_name                    = "${var.key_name}"
   associate_public_ip_address = "true"
-  security_groups             = ["${aws_security_group.docker.id}"]
+  security_groups             = ["${aws_security_group.docker.name}"]
 
 
   provisioner "remote-exec" {
